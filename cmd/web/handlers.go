7681b6d011b1094c114ea3e7d9970ca5c7b5d15b
cpustejovsky <charles.pustejovsky@gmail.com>
2020-12-27 17:12:08 -0500

    Wired up CheckIPAddress to handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,19 +3,22 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/cpustejovsky/microservice"
 	"github.com/cpustejovsky/microservice/internal/logger"
 )
 
 func (app *application) HelloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World"))
 }
-
+//TODO: change name of microservices.CheckIPAddress to clarify which is which?
 func (app *application) CheckIPAddress(w http.ResponseWriter, r *http.Request) {
 	logger.Info.Println(r.Body)
 	decoder := json.NewDecoder(r.Body)
 	type FormData struct {
-		IP string `json:"ip"`
+		IP        string   `json:"ip"`
+		Whitelist []string `json:"whitelist`
 	}
 	var data FormData
 	err := decoder.Decode(&data)
@@ -23,6 +26,7 @@ func (app *application) CheckIPAddress(w http.ResponseWriter, r *http.Request) {
 		logger.Error.Println(err)
 		http.Error(w, http.StatusText(404), http.StatusNotFound)
 	}
-	logger.Info.Println(data)
-	w.Write([]byte(data.IP))
+	ok := microservice.VerifyIPAddress(data.IP, data.Whitelist)
+
+	w.Write([]byte(strconv.FormatBool(ok)))
 }
