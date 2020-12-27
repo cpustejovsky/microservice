9ba0ac55900b073b45edfc8246d9d90a0668ee55
cpustejovsky <charles.pustejovsky@gmail.com>
2020-12-27 13:25:16 -0500

    Create ip internal package & FindCountryByIP func

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/cpustejovsky/microservice/logger"
+import (
+	"github.com/cpustejovsky/microservice/internal/ip"
+	"github.com/cpustejovsky/microservice/internal/logger"
+)
 
 func main() {
 	logger.Info.Println("Hello, World!")
+	country := ip.FindCountryByIP("81.2.69.142")
+	logger.Info.Println("COUNTRY:\t", country)
 }
--- internal/ip/ip.go
+++ internal/ip/ip.go
@@ -1,2 +1,24 @@
 package ip
 
+import (
+	"log"
+	"net"
+
+	"github.com/cpustejovsky/microservice/internal/logger"
+	"github.com/oschwald/geoip2-golang"
+)
+
+func FindCountryByIP(ipaddr string) string{
+	db, err := geoip2.Open("/home/cpustejovsky/development/microservice/internal/ip/GeoLite2-Country.mmdb")
+	if err != nil {
+		logger.Error.Fatal(err)
+	}
+	defer db.Close()
+	// If you are using strings that may be invalid, check that ip is not nil
+	ip := net.ParseIP(ipaddr)
+	record, err := db.Country(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return record.Country.Names["en"]
+}
