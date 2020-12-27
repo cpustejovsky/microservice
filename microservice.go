58f4ddfede806fa7c2cc94933aabfc527858e91f
cpustejovsky <charles.pustejovsky@gmail.com>
2020-12-27 15:00:15 -0500

    Got test passing

--- microservice.go
+++ microservice.go
@@ -1,6 +1,20 @@
 package microservice
 
+import (
+	"github.com/cpustejovsky/microservice/internal/ip"
+	"github.com/cpustejovsky/microservice/internal/logger"
+)
+
 //CheckIPAddress takes an IP address as a string and a whitelist as a slice of strings. it returns a bool based on whether the string exists
 func CheckIPAddress(ipaddr string, whitelist []string) bool {
+	country, err := ip.FindCountryByIP(ipaddr)
+	if err != nil {
+		logger.Error.Println(err)
+	}
+	for _, v := range whitelist {
+		if v == country {
+			return true
+		}
+	}
 	return false
 }
