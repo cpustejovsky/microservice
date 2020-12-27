c19cceae116184ec77e58b74cb6a44b6ec998365
cpustejovsky <charles.pustejovsky@gmail.com>
2020-12-27 15:02:08 -0500

    Add test for returning false when no IP address found

--- microservice_test.go
+++ microservice_test.go
@@ -9,14 +9,28 @@ import (
 
 func TestCheckIPAddress(t *testing.T) {
 	testIPAddr := "81.2.69.142"
-	testWhiteList := []string{
-		"United Kingdom",
-		"United States",
-		"Mexico",
-	}
+	t.Run("Returns true when IP address is included", func(t *testing.T) {
+		testWhiteList := []string{
+			"United Kingdom",
+			"United States",
+			"Mexico",
+		}
 
-	want := true
-	got := microservice.CheckIPAddress(testIPAddr, testWhiteList)
+		want := true
+		got := microservice.CheckIPAddress(testIPAddr, testWhiteList)
 
-	test.Compare(t, got, want)
+		test.Compare(t, got, want)
+	})
+	t.Run("Returns true when IP address is included", func(t *testing.T) {
+		testWhiteList := []string{
+			"United States",
+			"Mexico",
+		}
+
+		want := false
+		got := microservice.CheckIPAddress(testIPAddr, testWhiteList)
+
+		test.Compare(t, got, want)
+
+	})
 }
